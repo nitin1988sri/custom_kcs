fafb3b24d27eb9b8f33e1c20cf9326d3d8aa394c
saurabh <saurabh.srivastava.mca@gmail.com>
2025-05-26 10:05:13 +0530

    added fixture for state and city

--- custom_kcs/hooks.py
+++ custom_kcs/hooks.py
@@ -40,10 +40,11 @@ after_migrate = ["custom_kcs.src.custom_fields.add_fields_employee_checkIn.run_a
                     "custom_kcs.src.custom_fields.create_minimum_wage_doctype.run_all",  
                     "custom_kcs.src.custom_fields.create_state_doctype.run_all",
                     "custom_kcs.src.custom_fields.create_city_doctype.run_all",    
-                    #"custom_kcs.src.patches.city_state_bulk_insert.run"          
                 ]   
 
 boot_session = "custom_kcs.src.patches.override_monthly_attendance.boot"
+
+fixtures = ["State", "City"]
 
 # patches = [
 #     "custom_kcs.patches.city_state_bulk_insert.run"
